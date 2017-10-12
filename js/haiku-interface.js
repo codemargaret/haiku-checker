b00ba70e69b40a02e1d3b83513d71dfc826d53f3
Gabi Gavric and Margaret Berry <gabi@>
2017-10-12 16:43:17 -0700

    we addmited defeat its not applying the show and hide css stuff

--- js/haiku-interface.js
+++ js/haiku-interface.js
@@ -1,15 +1,15 @@
 import { Haiku, Poem } from './../js/haiku.js';
 
 $(document).ready(function(){
-  $('#haiku').submit(function(event){
+  $('.haiku').submit(function(event){
     event.preventDefault();
     var line1 = $('#line1').val();
     $('#entries').text(line1);
     newHaiku = new Haiku("haiku", line1)
     if (newHaiku.vowelCount() === 17) {
-      $('#yeshaiku').show();
+      $('.yeshaiku').show();
     } else {
-      $('#nohaiku').show();
+      $('.nohaiku').show();
     }
   });
 });
