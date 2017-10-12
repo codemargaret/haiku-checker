586aa12dd7137d3eb7e36483886fe4083f733564
Gabi Gavric and Margaret Berry <gabi@>
2017-10-12 16:37:55 -0700

    add partially working frontend

--- js/haiku-interface.js
+++ js/haiku-interface.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
   $('#haiku').submit(function(event){
     event.preventDefault();
     var line1 = $('#line1').val();
-
     $('#entries').text(line1);
+    newHaiku = new Haiku("haiku", line1)
+    if (newHaiku.vowelCount() === 17) {
+      $('#yeshaiku').show();
+    } else {
+      $('#nohaiku').show();
+    }
   });
 });
